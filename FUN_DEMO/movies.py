c5cd64f1df603d2ef18c2f4b341d7e0fca96b922
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2024-05-08 09:09:06 +0100

    Update movies.py

--- FUN_DEMO/movies.py
+++ FUN_DEMO/movies.py
@@ -9,8 +9,8 @@
     Download and display the Top 250 Movies Data from IMDB.
 """
 import sys
-import imdb
-
+# import imdb
+from PyMovieDb import IMDB
 import re
 
 movie_menu = """
@@ -22,14 +22,47 @@ movie_menu = """
 """
 
 def get_movies():
-    """ Get the top 250 movies online from IMDb """
-    ia = imdb.IMDb()
-    movies = {}
+    """ Get the top 50 movies online from IMDb """
+    ia = IMDB()
+    #  ia = imdb.IMDb()
+    ranked_movies = {}
+    # Returns 50 movies in a single string with lots of newline (\n) characters and header information
+    pop_movies = ia.popular_movies(genre=None, start_id=1, sort_by=None)
+    # Clean up the data by removing newline chars
+    pop_movies = pop_movies.replace("\n", "")
+    # Clean up the data by removing header info (by searching for the index of the first square bracket ([)
+    index = pop_movies.find("[",1)
+    # Slice the data to leave just the movies (the -1 ensures the final closing curly brace is also removed.
+    pop_movies = pop_movies[index:-1]
+    # pop_movies should be a string that has the structure of a list of dictionaries "[{a:b, c:d,..}{...}{...}]"
+    # Python’s eval() allows you to evaluate arbitrary Python expressions from a string-based or
+    # compiled-code-based input. This function can be handy when you’re trying to dynamically evaluate Python
+    # expressions from any input that comes as a string or a compiled code object.
+    # (https://realpython.com/python-eval-function/)
+    # So, convert the string to become a list of dictionaries
+    movies = list(eval(pop_movies))
+    # The database used by the API host appears to be a bit corrupt because amongst the "good" data it returns a lot
+    # of repeated rows (e.g. currently (06/02/2024) the film called "Deep Fear" is repeated 18 times at the end of the result
+    # set.
+    # So, the following block filters out any films with the same id
+    films = []
+    i = 0
+    while i < len(movies):
+        if films.count(movies[i]["id"]) == 0:  # see if film is already in list
+            films.append(movies[i]["id"])  # Create growing list of id's
+        else:
+            print(movies[i])  # prints repeated movie's details
+            del movies[i]
+            i -= 1
+        i += 1
 
-    for rank, movie in enumerate(ia.get_top250_movies(), start=1):
-        movies[rank] = str(movie)
+    # Generate ranked list of movie names
+    for rank, movie in enumerate(movies, start=1):
+        ranked_movies[rank] = movie["name"]
+        if rank >= 50:
+            break
 
-    return movies
+    return ranked_movies
 
 def display_movies(movies, top):
     """ Display top movies in a given dict """
@@ -38,8 +71,10 @@ def display_movies(movies, top):
     else:
         for rank, name in movies.items():
             print(f"{rank:>4d}: {name:<30s}")
-            if int(rank) == int(top): break
+            if int(rank) == int(top):
+                break
     return None
+
 
 def search_movies(pattern=r".", movies=None):
     """ Search for pattern in a given dict of movies """
@@ -54,6 +89,7 @@ def search_movies(pattern=r".", movies=None):
         print("No movies to search")
     return None
 
+
 def menu():
     """ Movie Menu """
     films = {}
@@ -63,9 +99,10 @@ def menu():
         if option == "1":
             films = get_movies()
         elif option == "2":
-            max = input("How many top movies do you want [default=250]: ")
-            if not max: max = 250
-            display_movies(films, max)
+            maxi = input("How many top movies do you want [default=50]: ")
+            if not maxi:
+                maxi = 50
+            display_movies(films, maxi)
         elif option == "3":
             pattern = input("Enter Regex search pattern: ")
             search_movies(pattern, films)
@@ -76,9 +113,11 @@ def menu():
 
     return None
 
+
 def main():
     menu()
     return None
+
 
 if __name__ == "__main__":
     main()
